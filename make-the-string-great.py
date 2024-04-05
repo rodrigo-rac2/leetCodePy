48f6b511e9a46eeb854074c72aad2be0d0c3a603
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2024-04-04 22:05:24 -0300

    Update make-the-string-great.py

--- make-the-string-great.py
+++ make-the-string-great.py
@@ -2,16 +2,22 @@
 
 class Solution:
     def makeGood(self, s: str) -> str:
-        l_s_pre = len(s)
-        l_s_post = -1
-        while l_s_pre != l_s_post:
-            l_s_pre = len(s)
-            if l_s_pre < 2: return s
-            if abs(ord(s[l_s_pre - 2]) - ord(s[l_s_pre - 1])) == 32:
-                s = s[:l_s_pre - 2]
-            for i in range(l_s_pre - 2):
-                if abs(ord(s[i]) - ord(s[i+1])) == 32:
-                    s = s[:i] + s[i+2:]
-                    break
-            l_s_post = len(s)
-        return s
+        stack = []
+        for i in range(len(s)):
+            l_s = len(stack)
+            if l_s > 0 and abs(ord(s[i]) - ord(stack[l_s - 1])) == 32: stack.pop()
+            else: stack.append(s[i])
+        return ''.join(stack)
+        # l_s_pre = 0
+        # l_s_post = -1
+        # while l_s_pre != l_s_post:
+        #     l_s_pre = len(s)
+        #     if l_s_pre < 2: return s
+        #     if abs(ord(s[l_s_pre - 2]) - ord(s[l_s_pre - 1])) == 32:
+        #         s = s[:l_s_pre - 2]
+        #     for i in range(l_s_pre - 2):
+        #         if abs(ord(s[i]) - ord(s[i+1])) == 32:
+        #             s = s[:i] + s[i+2:]
+        #             break
+        #     l_s_post = len(s)
+        # return s
