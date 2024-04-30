03ca85461e9887dff942cc19ad5e62e3a489ed51
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2024-04-30 15:46:48 -0300

    Update reverse-linked-list.py

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -7,19 +7,44 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head: return head
-        answer_l = []
+        answer = None
         current = head
-        while (current):
-            answer_l.append(current.val)
-            current = current.next
-        print(answer_l)
-        answer = ListNode()
-        answer_h = answer
-        length = len(answer_l)
-        for i in range(length):
-            answer.val = answer_l[length - 1 - i]
-            if i < length - 1:
-                answer.next = ListNode()
-                answer = answer.next
-        return answer_h
+        while current:
+            next_n = current.next
+            current.next = answer
+            answer = current
+            current = next_n
+        
+        return answer
+Ï
+        # Solution 2
+        # if not head: return head
+        # answer_l = []
+        # while head:
+        #     answer_l.append(head.val)
+        #     head = head.next
+        # answer = ListNode()
+        # runner = answer
+        # answer_l = answer_l[::-1]
+        # for i in range(len(answer_l)):
+        #     runner.val = answer_l[i]
+        #     if i < len(answer_l) - 1:
+        #         runner.next = ListNode()
+        #         runner = runner.next
+        # return answer
+        
+        # Solution 3:
+        # answer_l = []
+        # current = head
+        # while (current):
+        #     answer_l.append(current.val)
+        #     current = current.next
+        # answer = ListNode()
+        # answer_h = answer
+        # length = len(answer_l)
+        # for i in range(length):
+        #     answer.val = answer_l[length - 1 - i]
+        #     if i < length - 1:
+        #         answer.next = ListNode()
+        #         answer = answer.next
+        # return answer_h
