238d4c3d1dee6936a2d403bdb59ab8afeb33cc06
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2024-05-03 02:10:38 -0300

    Update remove-linked-list-elements.py

--- remove-linked-list-elements.py
+++ remove-linked-list-elements.py
@@ -7,14 +7,28 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        new_head = None
         while head:
-            if head.val != val: 
-                if not new_head:
-                    new_head = new_runner = ListNode(head.val)
-                else: 
-                    new_runner.next = ListNode()
-                    new_runner = new_runner.next
-                    new_runner.val = head.val
-            head = head.next
-        return new_head
+            if head.val == val:
+                head = head.next
+            else:
+                break
+        tail = head
+        while tail:
+            if tail.next and tail.next.val == val:
+                if tail.next.next:
+                    tail.next = tail.next.next
+                else:
+                    tail.next = None
+            else: tail = tail.next
+        return head
+        # new_head = None
+        # while head:
+        #     if head.val != val: 
+        #         if not new_head:
+        #             new_head = new_runner = ListNode(head.val)
+        #         else: 
+        #             new_runner.next = ListNode()
+        #             new_runner = new_runner.next
+        #             new_runner.val = head.val
+        #     head = head.next
+        # return new_head
